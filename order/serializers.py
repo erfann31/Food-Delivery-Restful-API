842249f638f6c42954b8ca1e8eb627816c85b489
erfann31 <erfannasri2@gmail.com>
2023-12-31 06:07:50 +0330

    add serializers for order app

---
+++ order/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from address.serializers import AddressSerializer
+from food.serializers import FoodSerializer
+from user.serializers import CustomUserSerializer
+from .models import Order, OrderItem
+
+
+class OrderItemsSerializer(serializers.ModelSerializer):
+    food = FoodSerializer()
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        read_only_fields = ('id',)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = OrderItemsSerializer()
+    user = CustomUserSerializer()
+    delivery_address = AddressSerializer()
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ('id', 'date_and_time', 'estimated_arrival')
