7e80557b4a115bd1c9232101fac3b666738d32b9
erfann31 <erfannasri2@gmail.com>
2024-01-01 08:18:04 +0330

    fix and test food app

--- food/serializers.py
+++ food/serializers.py
@@ -1,13 +1,42 @@
 from rest_framework import serializers
 
-from restaurant.serializers import RestaurantSerializer
+from restaurant.models import Restaurant
+from restaurant.serializers import RestaurantDetailSerializer, RestaurantIdSerializer
 from .models import Food
 
 
 class FoodSerializer(serializers.ModelSerializer):
-    restaurant = RestaurantSerializer()
+    restaurant = RestaurantDetailSerializer(read_only=True)  # For GET request
+    restaurant_id = RestaurantIdSerializer(write_only=True)  # For POST request
 
     class Meta:
         model = Food
         fields = '__all__'
-        read_only_fields = ('id', 'stars_count', 'stars')
+
+
+class FoodCreateSerializer(serializers.ModelSerializer):
+    restaurant_id = serializers.PrimaryKeyRelatedField(
+        queryset=Restaurant.objects.all(),
+        write_only=True,
+        source='restaurant'
+    )
+
+    class Meta:
+        model = Food
+        fields = [
+            'restaurant_id',
+            'name',
+            'price',
+            'min_time_to_delivery',
+            'max_time_to_delivery',
+            'category',
+        ]
+
+
+# Serializer for Food retrieval
+class FoodRetrieveSerializer(serializers.ModelSerializer):
+    restaurant = RestaurantDetailSerializer()
+
+    class Meta:
+        model = Food
+        fields = '__all__'
--- food/views.py
+++ food/views.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from food.models import Food
-from food.serializers import FoodSerializer
+from food.serializers import FoodSerializer, FoodCreateSerializer, FoodRetrieveSerializer
 
 
 class FoodViewSet(ModelViewSet):
     queryset = Food.objects.all()
-    serializer_class = FoodSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create' or self.action == 'update':
+            return FoodCreateSerializer
+        elif self.action == 'retrieve':
+            return FoodRetrieveSerializer
+        return FoodSerializer
