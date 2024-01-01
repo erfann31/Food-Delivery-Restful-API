faa5bc0c266b2fe87652965a9c98079785092c03
erfann31 <erfannasri2@gmail.com>
2024-01-01 03:58:11 +0330

    fix user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -9,11 +9,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
     favorite_restaurants = RestaurantSerializer()
     favorite_foods = FoodSerializer()
     password = serializers.CharField(write_only=True)
-    verification_token = serializers.CharField(write_only=True)
-    password_reset_token = serializers.CharField(write_only=True)
-    password_reset_token_created_at = serializers.DateTimeField(write_only=True)
 
     class Meta:
         model = CustomUser
         fields = '__all__'
-        read_only_fields = ('id', 'date_joined', 'verified', 'is_active', 'is_staff')
+        read_only_fields = ('id', 'date_joined', 'verified', 'is_active', 'is_staff', 'password_reset_token_created_at', 'password_reset_token', 'verification_token',)
