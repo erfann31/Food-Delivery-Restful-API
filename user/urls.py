6a413dc14acaa9beb49cadf697d30c55dc808822
erfann31 <erfannasri2@gmail.com>
2024-01-01 08:18:46 +0330

    delete delete api for use :)

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('verify/<str:token>/', views.email_verification_view, name='email-verification'),
-    path('delete/', views.delete_user, name='delete'),
     path('password-reset/', views.password_reset_request, name='password-reset'),
     path('password-reset/<str:token>/', views.password_reset_confirm, name='password-reset-confirm'),
     path('add_to_favorites/', views.add_to_favorites, name='add_to_favorites'),
