9c1319b4029f37a2622854e5676379ecf48a4da6
erfann31 <erfannasri2@gmail.com>
2024-01-01 08:18:20 +0330

    fix and test Restaurant app

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 
 from .models import Restaurant
 
+class RestaurantDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
 
+# Serializer for POST request
+class RestaurantIdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id']
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
