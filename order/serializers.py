61b92978738598c6542550bae9ee22b279864958
erfann31 <erfannasri2@gmail.com>
2024-01-02 04:31:20 +0330

    add food name category and price to order items of an order

--- order/serializers.py
+++ order/serializers.py
@@ -15,6 +15,17 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = ['food', 'quantity']
 
+    def to_representation(self, instance):
+        if isinstance(instance, OrderItem):
+            return {
+                'food_id': instance.food.id,
+                'food_name': instance.food.name,
+                'price': instance.food.price,
+                'category': instance.food.category,
+                'quantity': instance.quantity
+            }
+        return super().to_representation(instance)
+
 
 class OrderSerializer(serializers.ModelSerializer):
     delivery_address = serializers.PrimaryKeyRelatedField(queryset=Address.objects.all())
