7da616aa14a0b906acb58046f50db1d92839bf8f
erfann31 <erfannasri2@gmail.com>
2024-01-03 04:45:54 +0330

    fix reset password link generator

--- user/models.py
+++ user/models.py
@@ -86,6 +86,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def get_verification_url(self):
         return settings.BASE_URL + reverse('email-verification', kwargs={'token': self.verification_token})
 
+    def get_reset_url(self):
+        return settings.BASE_URL + reverse('password-reset-confirm', kwargs={'token': self.password_reset_token})
+
+
     def verification_token_expired(self):
         expiration_duration = timedelta(minutes=2)
         token_parts = self.verification_token.split('_')
@@ -105,12 +109,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         expiration_time = self.password_reset_token_created_at + expiration_duration
         return expiration_time <= timezone.now()
 
-    def send_password_reset_email(user):
+    def send_password_reset_email(self):
         subject = 'Password Reset Request'
-        reset_url = settings.BASE_URL + f'/password-reset/{user.password_reset_token}/'
+        reset_url = self.get_reset_url()
         message = render_to_string('password_reset_email_template.html', {'reset_url': reset_url})
         plain_message = strip_tags(message)
         from_email = settings.EMAIL_HOST_USER
-        to = user.email
+        to = self.email
 
         send_mail(subject, plain_message, from_email, [to], html_message=message)
