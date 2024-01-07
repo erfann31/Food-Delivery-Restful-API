8e341bcc0956fb669619aa5dc6b4933d35ff3804
erfann31 <erfannasri2@gmail.com>
2024-01-07 07:58:42 +0330

    fix order model

--- order/models/order.py
+++ order/models/order.py
@@ -9,7 +9,7 @@ from user.models import CustomUser
 
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, related_name='orders', on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True,default=0)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=ONGOING)
     date_and_time = models.DateTimeField(auto_now_add=True)
     delivery_address = models.ForeignKey(Address, related_name='orders', on_delete=models.CASCADE)
