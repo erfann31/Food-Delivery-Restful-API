3ba88ff1e88cbffcc9a23f618ce3104ec5e3f06f
erfann31 <erfannasri2@gmail.com>
2024-01-08 07:36:04 +0330

    fix requirements.txt

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 
 # exit on error
 set -o errexit
-
+pip install --upgrade pip
 pip install -r requirements.txt
 
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 python manage.py migrate
 #python manage.py runserver
