6b8f20308be4b4367c8702dd82ef213f91a0b6bd
erfann31 <erfannasri2@gmail.com>
2024-01-23 10:57:13 +0330

    update food app admin

--- food/admin.py
+++ food/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
+from food.forms.forms import FoodAdminForm
 from food.models.food import Food
 
-admin.site.register(Food)
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    form = FoodAdminForm
+    list_display = ['name', 'price', 'stars', 'stars_count', 'min_time_to_delivery', 'max_time_to_delivery', 'category', 'restaurant']
+    search_fields = ['name', 'category', 'restaurant']
+    list_filter = ['category', 'restaurant']
