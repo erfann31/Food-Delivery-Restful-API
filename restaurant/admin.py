7bd32c65da0817b11aa0b9be1a564e304cf0a118
erfann31 <erfannasri2@gmail.com>
2024-01-23 11:09:50 +0330

    update restaurant app admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from restaurant.models.restaurant import Restaurant
 
-admin.site.register(Restaurant)
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'stars', 'stars_count', 'distance', 'address', 'courier_price', 'opening_time']
+    search_fields = ['name', 'address']
+    list_filter = ['category']
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
