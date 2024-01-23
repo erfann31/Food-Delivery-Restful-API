47741f34d84b6d7c82599943c481ae96200ae4bb
erfann31 <erfannasri2@gmail.com>
2024-01-23 11:09:30 +0330

    add validations to restaurant app model

--- restaurant/models/restaurant.py
+++ restaurant/models/restaurant.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from consts.constants import CATEGORY_CHOICES, RESTAURANT_TAG_CHOICES
@@ -10,14 +10,18 @@ class Restaurant(models.Model):
     profile_picture = models.ImageField(upload_to='restaurant_profile_pics/', blank=True, null=True)
     tag1 = models.CharField(max_length=50, choices=RESTAURANT_TAG_CHOICES, blank=True, null=True)
     tag2 = models.CharField(max_length=50, choices=RESTAURANT_TAG_CHOICES, blank=True, null=True)
-    stars = models.FloatField(default=0,blank=True, null=True)
-    stars_count = models.IntegerField(default=0,blank=True, null=True)
-    distance = models.FloatField(default=0,blank=True, null=True)
+    stars = models.FloatField(default=0, blank=True, null=True, validators=[MinValueValidator(1),
+                                                                            MaxValueValidator(5)])
+    stars_count = models.IntegerField(default=0, blank=True, null=True, validators=[MinValueValidator(1), ])
+    distance = models.FloatField(default=0, blank=True, null=True, validators=[MinValueValidator(0.1),
+                                                                               MaxValueValidator(5)])
     address = models.CharField(max_length=256)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     name = models.CharField(max_length=50)
-    courier_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    opening_time = models.DecimalField(default=0, max_digits=4, decimal_places=2, validators=[MaxValueValidator(24)])
+    courier_price = models.DecimalField(max_digits=6, decimal_places=2, default=0, validators=[MinValueValidator(0),
+                                                                                               MaxValueValidator(999999)])
+    opening_time = models.DecimalField(default=0, max_digits=4, decimal_places=2, validators=[MinValueValidator(0),
+                                                                                              MaxValueValidator(24)])
 
     def __str__(self):
         return f"{self.name}"
