05fa3c49d809bf1fb314e45eb50832a24453b33f
erfann31 <erfannasri2@gmail.com>
2024-01-23 10:52:32 +0330

    update validate_time_range.py

--- food/utils/validate_time_range.py
+++ food/utils/validate_time_range.py
@@ -2,5 +2,9 @@ from django.core.exceptions import ValidationError
 
 
 def validate_time_range(min_time, max_time):
-    if min_time >= max_time or max_time - min_time >= 60:
-        raise ValidationError("Invalid time range")
+    if min_time >= max_time:
+        raise ValidationError("Minimum time must be less than maximum time")
+    elif max_time - min_time >= 60:
+        raise ValidationError("Time range must be less than 60 minutes")
+    elif max_time - min_time < 15:
+        raise ValidationError("Time range must be at least 15 minutes")
