c58dd05bf26b42dcdd98f0eb66cc9146cdf06e47
erfann31 <erfannasri2@gmail.com>
2024-01-23 11:00:30 +0330

    update update discount_code app admin

--- discount_code/admin.py
+++ discount_code/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from discount_code.models.discount_code import DiscountCode
 
-admin.site.register(DiscountCode)
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    list_display = ['code_text', 'discount_percent', 'is_active']
+    search_fields = ['code_text']
+    list_filter = ['is_active']
+
+admin.site.register(DiscountCode, DiscountCodeAdmin)
