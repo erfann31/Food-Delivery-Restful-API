25c1d31c088cd58b397c204b3e24f60c14520656
erfann31 <erfannasri2@gmail.com>
2024-01-23 11:47:16 +0330

    update restaurant app models test

--- restaurant/tests/models/test_restaurant.py
+++ restaurant/tests/models/test_restaurant.py
@@ -33,8 +33,8 @@ class TestRestaurantModel(unittest.TestCase):
     def test_save_method_with_generate_random_stars_count(self):
         restaurant = Restaurant.objects.create(name='Test Restaurant', category='Test Category', address='Test Address')
         restaurant.save()
-        lower_bound = 500.0
-        upper_bound = 10000.0
+        lower_bound = 1.0
+        upper_bound = 1000000.0
 
         self.assertGreaterEqual(restaurant.stars_count, lower_bound)
         self.assertLessEqual(restaurant.stars_count, upper_bound)
@@ -42,7 +42,7 @@ class TestRestaurantModel(unittest.TestCase):
     def test_save_method_with_generate_random_distance(self):
         restaurant = Restaurant.objects.create(name='Test Restaurant', category='Test Category', address='Test Address')
         restaurant.save()
-        lower_bound = 0.3
+        lower_bound = 0.1
         upper_bound = 5
 
         self.assertGreaterEqual(restaurant.distance, lower_bound)
