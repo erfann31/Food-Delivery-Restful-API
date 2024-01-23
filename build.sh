7e7c6ca30820cc1278b569d40ed9926497522ba7
erfann31 <erfannasri2@gmail.com>
2024-01-23 12:08:05 +0330

    fix sth

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+#python manage.py makemigrations
 python manage.py migrate
 #python manage.py runserver
