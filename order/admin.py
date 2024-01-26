d84ce342c5fae8ccc02d68a5927c6f0f9ef7c759
erfann31 <erfannasri2@gmail.com>
2024-01-26 10:40:12 +0330

    fix search field and a bug in order admin panel

--- order/admin.py
+++ order/admin.py
@@ -6,12 +6,12 @@ from order.models.order import Order
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'total_price', 'status', 'is_canceled', 'date_and_time', 'delivery_address', 'discount_code', 'estimated_arrival', ]
     list_filter = ['status', 'is_canceled']
-    search_fields = ['user__email', 'delivery_address__street_address', 'date_and_time']
+    search_fields = ['user__email', 'id']
     actions = ['cancel_orders', 'mark_as_delivered']
     readonly_fields = ['id', 'user', 'total_price', 'date_and_time', 'delivery_address', 'discount_code', 'estimated_arrival', ]
 
     def cancel_orders(self, request, queryset):
-        queryset.update(status='CANCELED', is_canceled=True)
+        queryset.update(status='Completed', is_canceled=True)
 
     cancel_orders.short_description = "Cancel selected orders"
 
